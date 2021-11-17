0488e60106810605a3b2aff7e7e28750960f04b4
Edwin Zapata <edwinrzc@gmail.com>
2021-11-17 12:15:42 -0300

    Se agrego la fecha y  el reloj

---
+++ src/components/EntryCode.vue
@@ -0,0 +1,85 @@
+<template>
+  <header class="masthead">
+    <div class="container">
+      <div class="masthead-subheading">Bienvenido a contacto Garantido!</div>
+      <div>
+        <p>Último ingreso: {{lastPersonal}}</p>
+        <p>Hora: {{entryDate}}</p>
+      </div>      
+      <input      
+        v-if="!isSubmitting"  
+        v-model="code"
+        @keyup.capture="setEntryPersonal"
+        ref="code"
+        type="text"
+        name="code"
+      />
+      <button @click="setEntryPersonal">Enviar</button>
+    </div>
+  </header>
+</template>
+<script>
+import { setEntry } from "@/api/modules/entry.api";
+export default {
+  name: "Entry-code",
+  data() {
+    return {
+      isSubmitting: false,
+      code: "",
+      result: "",
+      lastPersonal: "",
+      entryDate: ""
+    };
+  },
+  mounted() {
+    //this.form.email.focus();
+    this.$refs["code"].focus();
+    const _this = this;
+    setInterval(function() {
+      _this.$refs["code"].focus();
+    },8000);
+  },
+  methods: {
+    setLastEntry()
+    {
+      this.code = "";
+      this.$refs["code"].focus();
+      this.entryDate = this.$moment(this.result.data.transaccion.created_at).format('DD/MM/YYYY h:mm:ss');
+      this.lastPersonal = this.result.data.usuario.nombre +' '+ this.result.data.usuario.apellido;
+    },
+    async setEntryPersonal() {
+
+      const _this = this;
+      if (this.code.length < 10) {
+        return false;
+      }
+
+      this.isSubmitting = true;
+      const data = { "code-search": this.code, 'ip-host': window.location.origin };
+
+      try {
+          
+          this.result = await setEntry(data);
+          if(typeof this.result !== undefined){
+            setTimeout(() => {
+              this.setLastEntry()
+            }, 100);
+          }  
+      } catch (error) {
+        
+        this.$notify({
+          group: "foo",
+          type: "error",
+          title: error.data.message,
+          duration: 6000,
+        });
+
+      } finally {        
+        this.isSubmitting = false;
+        this.code = "";
+        //_this.$refs["code"].focus();
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,89 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+      <div id="clock" class="mt-4 ms-auto px-12 py-lg-0">
+        <p class="date">{{ date }}</p>
+        <p class="time">{{ time }}</p>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "nav",
+  data() {
+    return {
+      time: "",
+      date: "",
+    };
+  },
+  mounted() {
+    setInterval(this.updateTime, 1000);
+    this.updateTime();
+  },
+  methods: {
+    zeroPadding(num, digit) {
+      let zero = "";
+      for (var i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
+    },
+    updateTime() {
+      let cd = new Date();
+      let week = ["DOM", "LUN", "MAR", "MIE", "JUE", "VIE", "SAB"];
+      this.time =
+        this.zeroPadding(cd.getHours(), 2) +
+        ":" +
+        this.zeroPadding(cd.getMinutes(), 2) +
+        ":" +
+        this.zeroPadding(cd.getSeconds(), 2);
+      this.date =
+        this.zeroPadding(cd.getDate(), 2) +
+        "/" +
+        this.zeroPadding(cd.getMonth() + 1, 2) +
+        "/" +
+        this.zeroPadding(cd.getFullYear(), 4) +
+        " " +
+        week[cd.getDay()];
+    },
+  },
+};
+</script>
+<style lang="css" scoped>
+html,
+body {
+  height: 100%;
+}
+body {
+  background: #0f3854;
+  background: radial-gradient(ellipse at center, #0a2e38 0%, #000000 70%);
+  background-size: 100%;
+}
+p {
+  margin: 0;
+  padding: 0;
+}
+#clock {
+  color: #ffffff;
+  text-align: center;
+  position: absolute;
+  float: right;
+  right: 1%;
+  top: 50%;
+}
+.time {
+    letter-spacing: 0.05em;
+    font-size: 20px;
+    padding: 5px 0;
+  }
+  .date {
+    letter-spacing: 0.1em;
+    font-size: 24px;
+  }
+  .text {
+    letter-spacing: 0.1em;
+    font-size: 12px;
+    padding: 20px 0 0;
+  }
+</style>
